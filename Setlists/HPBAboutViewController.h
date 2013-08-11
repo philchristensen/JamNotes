9b6d8fbfdc211e1d85bee63bb032683ce184b7aa
Phil Christensen <phil@bubblehouse.org>
2013-08-11 14:27:20 -0400

    added info screen

---
+++ Setlists/HPBAboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  HPBAboutViewController.h
+//  JamNotes
+//
+//  Created by Phil Christensen on 8/11/13.
+//  Copyright (c) 2013 Phil Christensen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPBAboutViewController : UIViewController
+
+- (IBAction)done:(id)sender;
+
+@end
