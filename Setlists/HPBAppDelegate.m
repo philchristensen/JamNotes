4f9ac36868e475a479d49f706bfe060467acab57
Phil Christensen <phil@bubblehouse.org>
2019-02-13 14:05:05 -0500

    version update, remove some cruft

--- Setlists/HPBAppDelegate.m
+++ Setlists/HPBAppDelegate.m
@@ -25,7 +25,7 @@ NSArray* hex2RGBArray(NSString* hexcolor){
     return @[@(r), @(g), @(b)];
 }
 
-UIColor* hex2UIColor(NSString* hexcolor, CGFloat alpha) {
+UIColor* hex2UIColor(NSString* hexcolor, CGFloat alpha){
     NSArray* rgb = hex2RGBArray(hexcolor);
     return [UIColor colorWithRed: [rgb[0] floatValue] / 255
                            green: [rgb[1] floatValue] / 255
@@ -39,23 +39,10 @@ UIColor* hex2UIColor(NSString* hexcolor, CGFloat alpha) {
 @synthesize managedObjectModel = _managedObjectModel;
 @synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    UINavigationController *navigationController;
-    
-    // Override point for customization after application launch.
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
-        UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
-        navigationController = [splitViewController.viewControllers lastObject];
-        splitViewController.delegate = (id)navigationController.topViewController;
-        
-        UINavigationController *masterNavigationController = splitViewController.viewControllers[0];
-        HPBMasterViewController *controller = (HPBMasterViewController *)masterNavigationController.topViewController;
-        controller.managedObjectContext = self.managedObjectContext;
-    } else {
-        navigationController = (UINavigationController *)self.window.rootViewController;
-        HPBMasterViewController *controller = (HPBMasterViewController *)navigationController.topViewController;
-        controller.managedObjectContext = self.managedObjectContext;
-    }
+- (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions {
+    UINavigationController* navigationController = (UINavigationController *)self.window.rootViewController;
+    HPBMasterViewController* controller = (HPBMasterViewController *)navigationController.topViewController;
+    controller.managedObjectContext = self.managedObjectContext;
     
     navigationController.navigationBar.translucent = NO;
     navigationController.navigationBar.barTintColor = hex2UIColor(@"372172", 1.0);
@@ -64,36 +51,30 @@ UIColor* hex2UIColor(NSString* hexcolor, CGFloat alpha) {
     return YES;
 }
 							
-- (void)applicationWillResignActive:(UIApplication *)application
-{
+- (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
+- (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
+- (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
+- (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
-{
+- (void)applicationWillTerminate:(UIApplication *)application {
     // Saves changes in the application's managed object context before the application terminates.
     [self saveContext];
 }
 
-- (void)saveContext
-{
+- (void)saveContext {
     NSError *error = nil;
     NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
     if (managedObjectContext != nil) {
@@ -110,8 +91,7 @@ UIColor* hex2UIColor(NSString* hexcolor, CGFloat alpha) {
 
 // Returns the managed object context for the application.
 // If the context doesn't already exist, it is created and bound to the persistent store coordinator for the application.
-- (NSManagedObjectContext *)managedObjectContext
-{
+- (NSManagedObjectContext *)managedObjectContext {
     if (_managedObjectContext != nil) {
         return _managedObjectContext;
     }
@@ -126,8 +106,7 @@ UIColor* hex2UIColor(NSString* hexcolor, CGFloat alpha) {
 
 // Returns the managed object model for the application.
 // If the model doesn't already exist, it is created from the application's model.
-- (NSManagedObjectModel *)managedObjectModel
-{
+- (NSManagedObjectModel *)managedObjectModel {
     if (_managedObjectModel != nil) {
         return _managedObjectModel;
     }
@@ -138,8 +117,7 @@ UIColor* hex2UIColor(NSString* hexcolor, CGFloat alpha) {
 
 // Returns the persistent store coordinator for the application.
 // If the coordinator doesn't already exist, it is created and the application's store added to it.
-- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
-{
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator {
     if (_persistentStoreCoordinator != nil) {
         return _persistentStoreCoordinator;
     }
